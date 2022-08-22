d6fa3cfdf5080b60ed71fa7896b096a49c97ce79
Chuck Japely <chuckjapely@gmail.com>
2022-08-21 21:51:53 -0700

    Initial Commit

---
+++ welcomeScreenapp/welcomeScreenappApp.swift
@@ -0,0 +1,17 @@
+//
+//  welcomeScreenappApp.swift
+//  welcomeScreenapp
+//
+//  Created by Chuck Japely on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct welcomeScreenappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
